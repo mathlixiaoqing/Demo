6887c38aa41622da7c415d0082329e2d379e3eb6
mathlixiaoqing <mathlxq0826@163.com>
2018-04-16 14:43:37 +0800

    测试还原01

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -3,6 +3,6 @@ package com.bfd;
 public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
-        System.out.println("测试还原");
+        System.out.println("测试还原01");
     }
 }
