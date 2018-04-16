250fbdf5d8309e8542c2a2b404a3f3a4f2f3b769
mathlixiaoqing <mathlxq0826@163.com>
2018-04-16 15:58:38 +0800

    old01

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -3,8 +3,6 @@ package com.bfd;
 public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
-        System.out.println("主干");
-        System.out.println("分支01");
-        System.out.println("分支02");
+        System.out.println("old01");
     }
 }
