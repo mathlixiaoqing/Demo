b3df0073c76398e684c01b09b0b389310926ab8a
mathlixiaoqing <mathlxq0826@163.com>
2018-04-16 15:45:40 +0800

    测试分支02

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
         System.out.println("分支01");
+        System.out.println("分支02");
     }
 }
