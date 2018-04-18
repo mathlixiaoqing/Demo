279e430e35fe77783991dc11bd9730c5a52918a1
mathlixiaoqing <mathlxq0826@163.com>
2018-04-18 18:36:12 +0800

    回滚01

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -3,9 +3,6 @@ package com.bfd;
 public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
-        System.out.println("测试01");
-
-
-
+        System.out.println("回滚01");
     }
 }
