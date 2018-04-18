ebe9a77e9512acb94dbd6f3ba402b499347c5245
mathlixiaoqing <mathlxq0826@163.com>
2018-04-18 18:21:40 +0800

    测试01

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -3,7 +3,7 @@ package com.bfd;
 public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
-        System.out.println("测试回滚单个文件");
+        System.out.println("测试01");
 
 
 
