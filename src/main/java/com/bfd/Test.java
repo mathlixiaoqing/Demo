23449ad8c3d9bbd2e4708b25e488a0392307670d
mathlixiaoqing <mathlxq0826@163.com>
2018-04-18 18:42:10 +0800

    分支

--- src/main/java/com/bfd/Test.java
+++ src/main/java/com/bfd/Test.java
@@ -3,7 +3,6 @@ package com.bfd;
 public class Test {
     public  static void  main(String args[]){
         System.out.println("haha");
-        System.out.println("分支01");
-        System.out.println("分支02");
+        System.out.println("分支");
     }
 }
